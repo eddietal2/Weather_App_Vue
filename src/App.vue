1496761ba1669586230ea7f610b7bd1b1d32188f
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-02-15 21:44:40 -0500

    Gutted out placeholder components/UI

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="150" height="150" />
-
     <div class="wrapper">
-      <HelloWorld msg="Whaddup Doe!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      
       </nav>
     </div>
   </header>
@@ -57,22 +52,6 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
   nav {
     text-align: left;
     margin-left: -1rem;
